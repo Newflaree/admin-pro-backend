b509f8711ea616b0eb9174c5cd0a925cf79cd535
Newflare <camiloj.lopez79@gmail.com>
2021-01-28 12:57:01 -0300

    Google Sign-in

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,10 @@
-const { response } = require( 'express' );
+const { response, request } = require( 'express' );
 const bcrypt = require( 'bcryptjs' );
 
 const Usuario = require( '../models/usuario' );
 const { generarJWT } = require( '../helpers/jwt' );
+const { googleVerify } = require('../helpers/google-verify');
+const usuario = require('../models/usuario');
 
 const login = async( req, res = response ) => {
   const { email, password } = req.body;
@@ -45,7 +47,54 @@ const login = async( req, res = response ) => {
   } 
 }
 
+const googleSignIn = async( req = request, res = response ) => {
+  const googleToken = req.body.token;
+
+  try {
+    const { name, email, picture } = await googleVerify( googleToken );
+
+    const usuarioDB = await Usuario.findOne({ email });
+    let usuario;
+
+    // Si no existe el usuario
+    if ( !usuarioDB ) {
+      usuario = new Usuario({
+        nombre: name,
+        email,
+        password: '@@@',
+        img: picture,
+        google: true
+        
+      });
+    } else {
+      // Existe usuario
+      usuario = usuarioDB;
+      usuario.google = true;
+    }
+
+    // Guardar en DB
+    await usuario.save();
+
+    // Generar el TOKEN - JWT
+    const token =  await generarJWT( usuario.id );
+
+    res.json({
+      ok: true,
+      token
+    });
+
+  } catch ( err ) {
+    console.log( err );
+    res.status( 401 ).json({
+      ok: false,
+      msg: 'Token no es correcto'
+    });
+  }
+  
+} 
+
 
 module.exports = {
-  login
+  login,
+  googleSignIn
 }
--- routes/auth.js
+++ routes/auth.js
@@ -3,18 +3,27 @@
  */
 
 const { Router } = require( 'express' );
-const { login } = require( '../controllers/auth' );
+const { login, googleSignIn } = require( '../controllers/auth' );
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
-router.post( '', 
+router.post( '/', 
   [
     check( 'email', 'El email es obligatorio' ).isEmail(), 
     check( 'password', 'El password es obligatorio' ).not().isEmpty(), 
     validarCampos
   ], 
-  login );
+  login 
+);
+
+router.post( '/google', 
+  [
+    check( 'token', 'El token de Google es obligatorio' ).not().isEmpty(), 
+    validarCampos
+  ], 
+  googleSignIn 
+);
 
 module.exports = router;
